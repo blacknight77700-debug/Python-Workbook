48de9275998c988b3ff3e84df0d73f889a8b4fdf
blacknight7700 <blacknight77700@gmail.com>
2026-09-01 15:02:40 -0500

    u

--- Domain 1/Student/114-analyze.py
+++ Domain 1/Student/114-analyze.py
@@ -1,5 +1,5 @@
-print(type("This is a quiz on data types"))
-print(type(True))
-print(type("20000"))
-print(type(2.22))
-print(type(20000))
+print(type("This is a quiz on data types")) # string
+print(type(True)) # boolean
+print(type("20000")) # string
+print(type(2.22)) # float
+print(type(20000)) # integer

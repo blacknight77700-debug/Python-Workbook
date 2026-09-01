183bc01f07622205197093ade082fa57f321f4ef
blacknight7700 <blacknight77700@gmail.com>
2026-09-01 15:04:40 -0500

    u
    Updated comments for clarity and added explanation about tuple.

--- Domain 1/Student/126-analyze.py
+++ Domain 1/Student/126-analyze.py
@@ -1,5 +1,5 @@
 quote = "The quick brown fox jumped over the lazy dog."
-print(quote[4:9])
+print(quote[4:9]) # quick
 
-coins = ('Platinum','Gold','Silver') #What type of data structure is this?
-#Can we change Platinum to Bronze?
+coins = ('Platinum','Gold','Silver') #What type of data structure is this? # tuple
+#Can we change Platinum to Bronze? # Yes, redeclare the coins variables like conins = ('Bronze','Gold','Silver')

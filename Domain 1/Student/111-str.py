d5b8f9e35426503fcaec8dadfff4f6bf1dec3e3f
blacknight7700 <blacknight77700@gmail.com>
2026-09-01 14:39:12 -0500

    Update
    Added print statement for full name and commented out an error example.

--- Domain 1/Student/111-str.py
+++ Domain 1/Student/111-str.py
@@ -1,2 +1,5 @@
 first_name = "Jason"
 last_name = "Manibog"
+print(first_name + " " + last_name)
+
+# print('Jason's turn) This does not work because Python rt=things we have an opening quote with no closing quote. This is why it generates an error message

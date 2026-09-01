13b92672ccd2549e799b3c24d89d8bab882b4b1f
blacknight7700 <blacknight77700@gmail.com>
2026-09-01 14:50:45 -0500

    Update

--- Domain 1/Student/122-indexing-completed.py
+++ Domain 1/Student/122-indexing-completed.py
@@ -1,3 +1,4 @@
 categories = ["Early Bronze Age", "Middle Bronze Age", "Late Bronze Age", "Bronze Age Collapse", "Early Iron Age"]
 print(categories)
 
+print(categories[-1]) # print(categories[0]) print(categories[2])

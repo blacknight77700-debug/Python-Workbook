905e8e640bd7b17cae50c367e5b6802b88a045ed
blacknight7700 <blacknight77700@gmail.com>
2026-09-02 14:01:36 -0500

    u

--- Domain 1/Student/111-str-completed.py
+++ Domain 1/Student/111-str-completed.py
@@ -2,4 +2,4 @@ first_name = "Jason"
 last_name = "Manibog"
 print(first_name + " " + last_name)
 
-# print('Jason's turn) This does not work because Python rt=things we have an opening quote with no closing quote. This is why it generates an error message
+# print('Jason's turn) This does not work because Python thinks we have an opening quote with no closing quote. This is why it generates an error message

e6cf39abaec231881d3e36df37aaf98e05bf8119
blacknight7700 <blacknight77700@gmail.com>
2026-09-02 13:46:12 -0500

    u

---
+++ Domain 2/Student/222-for-completed.py
@@ -0,0 +1,7 @@
+positions = 10
+for p in range(1,10+1,2): # When I add a comma and the number 2 at the end of the range and run the code. This is the output: You are in position 1 you in position 3 You are in position 5 You are in position 7 You are in position 9
+    print ("You are in position", p)
+
+for m in range(1,27):
+    print("You have reached mile", m)
+# It starts at position 1

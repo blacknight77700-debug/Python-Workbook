4f295d9a2d56a0be61dc41dac96f2a1f11f4bf1e
blacknight7700 <blacknight77700@gmail.com>
2026-09-02 13:50:16 -0500

    u

--- Domain 2/Student/224-continue.py
+++ Domain 2/Student/224-continue.py
@@ -1,7 +1,8 @@
 coins = ('Bronze','Silver','Platinum','Gold')
 for coin in coins:
-    print ('You possess a', coin, 'coin.')
+   
     if coin == 'Platinum':
         print('Congratulations! The platinum coin will move you to the next level!')
         continue
+    print ('You possess a', coin, 'coin.')
     

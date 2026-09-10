dfda57d7724bd6b3a5970f4ebdd2b7bfb36d21cf
blacknight7700 <blacknight77700@gmail.com>
2026-09-10 14:56:04 -0500

    u

--- Domain 3/Student/315-append-completed.py
+++ Domain 3/Student/315-append-completed.py
@@ -3,6 +3,6 @@ message.write('Testing file for player configuration\n')
 message.write('Testing file for player score\n')
 message.close()
 
-message = open('315-message.txt','w')
+message = open('315-message.txt','a')
 message.write('Spencer\n')
 message.close()
